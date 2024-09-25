6e1d5e69ae965ea8bbf5532589e99da0021a7151
Aditya Agarwal <adi.ag321@gmail.com>
2024-09-25 16:47:33 -0500

    Update Reading_csv_files.py

--- Reading_csv_files.py
+++ Reading_csv_files.py
@@ -20,9 +20,17 @@ def read_csv():
     device = int(input("Select 1 if you are working in Windows Laptop and 2 for MacBook: "))
 
     if device == 1:
-        os.chdir("D:/OneDrive - Northeastern University/Data-Science-datasets")
-        print("Location changed for Windows")
-        print("Updated Location is: D:/OneDrive - Northeastern University/Data-Science-datasets")
+        folder = int(input("Select 1 if the location of the dataset is 'Machine Learning Datasets' or 2 if Data science datasets")
+        
+        if folder == 1:
+            os.chdir("D:\OneDrive - Northeastern University\Jupyter Notebook\Machine Learning Algorithms\Datasets")
+            print("Location changed for Windows")
+            print("Updated Location is: D:\OneDrive - Northeastern University\Jupyter Notebook\Machine Learning Algorithms\Datasets")
+            
+        else:
+            os.chdir("D:/OneDrive - Northeastern University/Data-Science-datasets")
+            print("Location changed for Windows")
+            print("Updated Location is: D:/OneDrive - Northeastern University/Data-Science-datasets")
 
     elif device == 2:
         os.chdir("/Users/adityaagarwal/Library/CloudStorage/OneDrive-NortheasternUniversity/Data-Science-datasets/")
@@ -30,5 +38,8 @@ def read_csv():
         print("Updated Location is: /Users/adityaagarwal/Library/CloudStorage/OneDrive-NortheasternUniversity/Data-Science-datasets")
     else:
         print("Either enter 1 or 2")
+        
+        
+        
 #df = pd.read_csv("./Flight Status Prediction/Combined_Flights_2022.parquet")
 #df.head(3)
